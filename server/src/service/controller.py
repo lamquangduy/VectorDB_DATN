31c32ecc83a58c046d73df673ec63f0976e0a72a
Phạm Tân Tị <kurokid0206@gmail.com>
2024-06-07 10:18:53 +0700

    fix: syntax

--- server/src/service/controller.py
+++ server/src/service/controller.py
@@ -1,14 +1,16 @@
 from fastapi import APIRouter, UploadFile, Request
 from . import repository
 from pathlib import Path
+
 router = APIRouter()
 import os
 from pydantic import BaseModel
 
 
-UPLOAD_DIR = Path().resolve() / 'upload'
+UPLOAD_DIR = Path().resolve() / "upload"
 if not os.path.exists(UPLOAD_DIR):
     os.makedirs(UPLOAD_DIR)
+
 
 class ChatInput(BaseModel):
     text: str
@@ -25,22 +27,20 @@ async def chat(data: ChatInput):
     return result
 
 
-
-@router.post("/uploadfile/")
+@router.post("/upload-file")
 async def create_upload_file(file_upload: UploadFile):
     data = await file_upload.read()
     save_to = UPLOAD_DIR / file_upload.filename
-    with open(os.path.join(save_to), 'wb') as f:
+    with open(os.path.join(save_to), "wb") as f:
         f.write(data)
-    repository.embedding(repository.getformat(save_to)['split_name'][1],save_to)
-    print(repository.getformat(save_to)['split_name'][1])
+    repository.embedding(repository.get_format(save_to)["split_name"][1], save_to)
+    print(repository.get_format(save_to)["split_name"][1])
     return {"filenames": file_upload.filename}
 
 
-@router.post("/uploadurl/")
+@router.post("/upload-url")
 async def upload_url(request: Request):
     data = await request.json()
     url = data.get("url")
     repository.embedding_URL(url)
     return {"url": url}
-
