70b5dfcba4e78dd2eac5594ae6e96b475eaf28f6
Phạm Tân Tị <kurokid0206@gmail.com>
2024-06-17 20:16:30 +0700

    lint

--- frontend-v2/src/views/chat/AdminPage.tsx
+++ frontend-v2/src/views/chat/AdminPage.tsx
@@ -1,9 +1,9 @@
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import Import from "./components/Import";
 // import Footer from "./components/Footer";
 import { Box, createTheme, ThemeProvider, Typography } from "@mui/material";
-import { useEffect } from "react";
-import { useAuth0 } from "@auth0/auth0-react";
+// import { useEffect } from "react";
+// import { useAuth0 } from "@auth0/auth0-react";
 
 const theme = createTheme({
   typography: {
@@ -12,14 +12,14 @@ const theme = createTheme({
 });
 
 const ChatAdminPage = () => {
-  const {user}=useAuth0()
-  const navigate=useNavigate()
-  useEffect(()=>{
-    console.log(user)
-    if(user===null||user===undefined){
-      navigate("/login");
-    }
-  },[navigate, user])
+  // const { user } = useAuth0();
+  // const navigate = useNavigate();
+  // useEffect(() => {
+  //   console.log(user);
+  //   if (user === null || user === undefined) {
+  //     navigate("/login");
+  //   }
+  // }, [navigate, user]);
   return (
     <ThemeProvider theme={theme}>
       {/* <Header></Header> */}
